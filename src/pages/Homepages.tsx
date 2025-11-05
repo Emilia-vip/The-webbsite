a2ca5c6e698fb0059f8f50cb7369bd5d9eab1028
Emilia-vip <emiliaviperstrand@gmail.com>
2025-11-05 16:47:39 +0100

    final-sug

--- src/pages/Homepages.tsx
+++ src/pages/Homepages.tsx
@@ -4,9 +4,9 @@ import React from "react"
 function Homepages(){
     return <>
     <div className="info">
-    <h3>Välkommen!</h3>
+    <h3 className="h3">Välkommen!</h3>
     
-<p>
+<p className="p">
  Här får du en stund för dig själv – tid att slappna av, återhämta dig och ge kroppen den omtanke den förtjänar.
  Jag erbjuder professionell massage anpassad efter dina behov – oavsett om du söker avslappning, smärtlindring eller vill motverka spänningar efter arbete eller träning.
 Varmt välkommen att boka din behandling och låt mig hjälpa dig till en mjukare, starkare och mer balanserad kropp.
